17eccd39581001e6e80dc6b7871c62207ab688c7
Valeria Salas <visalas@uc.cl>
2023-10-17 01:09:09 -0300

    feat fetch data group reports

--- MIMIC-CXR-T/src/common/Translator.jsx
+++ MIMIC-CXR-T/src/common/Translator.jsx
@@ -1,24 +1,42 @@
-import React , { useState }from 'react';
+import React, { useEffect, useState } from 'react';
 import MyNavBar from '../Components/NavApp';
 import LeftViewer from '../Components/LeftViewer';
 import RightViewer from '../Components/RightViewer';
 import './translator.css';
+import axios from 'axios';
 
-function Translator() {
+function Translator({ match }) {
+  const { groupId } = match.params; // Extract groupId from the URL
+  const [groupData, setGroupData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/data/${groupId}`
+        );
+        setGroupData(response.data); // Assuming response.data contains the group data
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [groupId]);
 
   return (
     <>
-    <div>
-      <MyNavBar/>
-    </div>
-    <div className="app-container">
+      <div>
+        <MyNavBar />
+      </div>
+      <div className="app-container">
         <div className="leftviewer-container">
-          <LeftViewer />
+          <LeftViewer groupData={groupData} />
         </div>
         <div className="rightviewer-container">
-        < RightViewer />
+          <RightViewer />
         </div>
-    </div>
+      </div>
     </>
   );
 }
