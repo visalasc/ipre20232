64a0bea49339e1d79537b83b0aa3a6404a9f8ab1
Valeria Salas <visalas@uc.cl>
2023-10-17 05:53:47 -0300

    refactory navbarreportselect delete modals

--- MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
+++ MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
@@ -1,42 +1,18 @@
-import React, { useState } from 'react';
-import { Navbar, Nav, Button, Modal} from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { Navbar, Nav, Button, Modal } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
+import ModalDictionary  from '../Components/ModalDictionary';
+import ModalUploadReport from '../Components/ModalUploadReport';
+
 
 const NavBarReportSelection = () => {
-  const [showModal, setShowModal] = useState(false);
 
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  const handleLoadFile = () => {
-    // Lógica para cargar archivos
-  };
 
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Navbar.Brand href="/">MIMIC-CXR</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link onClick={handleShowModal}>Diccionario</Nav.Link>
-        </Nav>
-        <Button variant="primary" onClick={handleLoadFile}>
-          Cargar Reporte
-        </Button>
-      </Navbar.Collapse>
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Diccionario</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {/* Contenido del Modal */}
-        </Modal.Body>
-      </Modal>
+      <ModalUploadReport/>
+      <ModalDictionary/>
       <LogoutButton />
     </Navbar>
   );
