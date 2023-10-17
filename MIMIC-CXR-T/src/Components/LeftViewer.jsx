e78e408b99289f73cc116089c0fc623bbb5469ad
Valeria Salas <visalas@uc.cl>
2023-10-17 01:10:02 -0300

    feat  add groupdata to leftviewer

--- MIMIC-CXR-T/src/Components/LeftViewer.jsx
+++ MIMIC-CXR-T/src/Components/LeftViewer.jsx
@@ -1,40 +1,21 @@
-import axios from 'axios';
-import React , { useEffect, useState }from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-function LeftViewer() {
-
+function LeftViewer({ groupData }) {
   return (
-    <>
-     <div className="container mt-4">
+    <div className="container mt-4">
       <Card border="secondary" style={{ width: '25rem' }}>
         <Card.Body>
-        <Card.Header>Reporte original:</Card.Header>
-          <Card.Title>Warning Card Title</Card.Title>
-          <Card.Text id="1">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-             tempor incididunt ut labore et dolore magna aliqua. Mattis ullamcorper
-              velit sed ullamcorper. Et odio pellentesque diam volutpat commodo sed.
-          </Card.Text>
-          <Card.Text id="2">
-            Oracion 2: Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-             tempor incididunt ut labore et dolore magna aliqua. Mattis ullamcorper
-          </Card.Text>
-          <Card.Text id="3">
-            Oracion 3: Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-             tempor incididunt ut labore et dolore magna aliqua. Mattis ullamcorper
-          </Card.Text>
+          <Card.Header>Reporte original:</Card.Header>
+          {groupData &&
+            groupData.sentences.map((sentence, index) => (
+              <Card.Text key={index} id={sentence.id}>
+                {sentence.text}
+              </Card.Text>
+            ))}
         </Card.Body>
       </Card>
-      </div>
-
-    </>
+    </div>
   );
 }
 
