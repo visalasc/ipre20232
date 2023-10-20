48ccdb9ff3480825fa8c2bb88ae2459223bf8d0a
Valeria Salas <visalas@uc.cl>
2023-10-20 03:26:20 -0300

    feat report to phrase left viewer

--- MIMIC-CXR-T/src/Components/LeftViewer.jsx
+++ MIMIC-CXR-T/src/Components/LeftViewer.jsx
@@ -1,44 +1,27 @@
 import React , { useState, useEffect } from 'react';
 import { Button, Card, Container, Col, Row, ButtonGroup } from 'react-bootstrap';
-
-function LeftViewer({ reports }) {
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToNextReport = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % reports.length);
-  };
-
-  const goToPreviousReport = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + reports.length) % reports.length);
-  };
-
-  useEffect(() => {
-    setCurrentIndex(0);
-  }, [reports]);
-
+import './leftviewer.css'
+function LeftViewer({ reports , currentIndex, highlightedPhraseIndex, setHighlightedPhraseIndex}) {
+  const currentReport = reports[currentIndex];
+  console.log(currentReport);
   return (
     <>
-          <ButtonGroup size="lg" className="mb-2">
-            <Button variant="primary" onClick={goToPreviousReport}
-          disabled={reports.length <= 1}>
-            Anterior
-          </Button>
-      
-            <Button variant="primary" onClick={goToNextReport} 
-          disabled={reports.length <= 1}>
-            Siguiente
-          </Button>
-          </ButtonGroup>
-         
       <Card text="dark" 
       bg="light" border="secondary" 
       style={{ width: '23rem' , height: 'auto' , overflow: 'scroll'}}>
         <Card.Body>
           <Card.Header>Reporte original:</Card.Header>
-            <Card.Text>ID: {reports[currentIndex].id}</Card.Text>
-            <Card.Text>Created At: {reports[currentIndex].createdAt}</Card.Text>
-            <Card.Text>Contenido: {reports[currentIndex].text}</Card.Text>
+            <Card.Text>ReportID: {currentReport.id}</Card.Text>
+            
+                    {currentReport.phrases.map((phrase, index) => (
+                <Card.Text key={phrase.id} 
+                className={highlightedPhraseIndex === index ? 'highlighted-left' : ''}
+                onMouseEnter={() => setHighlightedPhraseIndex(index)}
+                onMouseLeave={() => setHighlightedPhraseIndex(null)}
+                >{phrase.text} 
+                  </Card.Text>
+              ))}
+           
         </Card.Body>
       </Card>
       </>
