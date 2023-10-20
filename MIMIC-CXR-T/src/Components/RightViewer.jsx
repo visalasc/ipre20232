975bfdab800cc017197c26e25d13eede27e0c45c
Valeria Salas <visalas@uc.cl>
2023-10-20 03:25:19 -0300

    feat rightviewer

--- MIMIC-CXR-T/src/Components/RightViewer.jsx
+++ MIMIC-CXR-T/src/Components/RightViewer.jsx
@@ -1,170 +1,31 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilAlt, faLightbulb } from '@fortawesome/free-solid-svg-icons';
-import { Row, Col, Button, Modal, Toast, Card } from 'react-bootstrap';
-
-function RightViewer() {
-  const [editableText, setEditableText] = useState([
-    {
-      id: '1',
-      text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      editedText: '',
-      isEditing: false,
-    },
-    {
-      id: '2',
-      text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      editedText: '',
-      isEditing: false,
-    },
-    {
-      id: '3',
-      text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      editedText: '',
-      isEditing: false,
-    },
-  ]);
-
-  const [showModal, setShowModal] = useState(false);
-  const [suggestion, setSuggestion] = useState('');
-  const [toasts, setToasts] = useState([]); // Almacena las sugerencias como objetos
-
-  const handleEditClick = (id) => {
-    const updatedText = editableText.map((item) => {
-      if (item.id === id) {
-        return { ...item, isEditing: true, editedText: item.text };
-      }
-      return item;
-    });
-    setEditableText(updatedText);
-  };
-
-  const handleSaveClick = (id) => {
-    const updatedText = editableText.map((item) => {
-      if (item.id === id) {
-        return { ...item, isEditing: false, text: item.editedText };
-      }
-      return item;
-    });
-    setEditableText(updatedText);
-  };
-
-  const handleSuggestionClick = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  const handleSuggestionSubmit = () => {
-    // Agregar la sugerencia como un objeto al array de toasts
-    setToasts([...toasts, { id: Date.now(), suggestion }]);
-    setShowModal(false);
-    setSuggestion(''); // Limpiar el campo de sugerencia
-  };
+import React , { useState, useEffect } from 'react';
+import { Button, Card, Container, Col, Row, ButtonGroup } from 'react-bootstrap';
+import './rightviewer.css'
+function RightViewer2({ translatedreports , currentIndex, highlightedPhraseIndex, setHighlightedPhraseIndex }) {
+  const currentTranslatedReport = translatedreports[currentIndex];
 
   return (
-  <>
-    <Col>
-      <Card border="info" style={{ width: '25rem' ,height:'100%'}}>
-        <Card.Header>Pre-traducción:</Card.Header>
+    <>         
+      <Card text="dark" 
+      bg="light" border="secondary" 
+      style={{ width: '23rem' , height: 'auto' , overflow: 'scroll'}}>
         <Card.Body>
-          <Card.Title>Warning Card Title</Card.Title>
-          {editableText.map((item) => (
-            <Card.Text key={item.id}>
-              {item.isEditing ? (
-                <>
-                  <input
-                    type="text"
-                    value={item.editedText}
-                    onChange={(e) => {
-                      const updatedText = e.target.value;
-                      const updatedEditableText = editableText.map((textItem) => {
-                        if (textItem.id === item.id) {
-                          return { ...textItem, editedText: updatedText };
-                        }
-                        return textItem;
-                      });
-                      setEditableText(updatedEditableText);
-                    }}
-                  />
-                  <button onClick={() => handleSaveClick(item.id)}>Guardar</button>
-                </>
-              ) : (
-                <>
-                  <span>
-                    {item.text.split(' ').map((word, index) => (
-                      <span
-                        key={index}
-                        style={{
-                          backgroundColor: item.editedText.includes(word) ? 'yellow' : 'transparent',
-                        }}
-                      >
-                        {index > 0 ? ' ' : ''}
-                        {word}
-                      </span>
-                    ))}
-                  </span>
-                  <Button variant="light" onClick={() => handleEditClick(item.id)}>
-                    <FontAwesomeIcon icon={faPencilAlt} />
-                  </Button>
-                  <Button variant="warning" onClick={() => handleSuggestionClick(item.id)}>
-                    <FontAwesomeIcon icon={faLightbulb} />
-                  </Button>
-                </>
-              )}
-            </Card.Text>
-          ))}
+          <Card.Header>Reporte Pre-traducido:</Card.Header>
+            <Card.Text>TranslatedReportID: {currentTranslatedReport.id}</Card.Text>
+              {currentTranslatedReport.translatedphrases.map((phrase, index) => (
+                  <Card.Text key={phrase.id} 
+                  className={highlightedPhraseIndex === index ? 'highlighted-right' : ''}
+                  onMouseEnter={() => setHighlightedPhraseIndex(index)}
+                  onMouseLeave={() => setHighlightedPhraseIndex(null)}        
+              >
+                    {phrase.text} 
+                    </Card.Text>
+                ))}
+           
         </Card.Body>
       </Card>
-
-      {/* Modal para sugerencias */}
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Escribe tu sugerencia</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <textarea
-            rows="4"
-            cols="50"
-            value={suggestion}
-            onChange={(e) => setSuggestion(e.target.value)}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleSuggestionSubmit}>
-            Enviar Sugerencia
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-</Col>
-  
-            <Col>
-              {/* Aquí mapea las sugerencias en Toasts */}
-              {toasts.map((toast) => (
-                <Toast
-                  key={toast.id}
-                  onClose={() => {
-                    // Eliminar la Toast cuando se cierre
-                    const updatedToasts = toasts.filter((t) => t.id !== toast.id);
-                    setToasts(updatedToasts);
-                  }}
-                  style={{ marginTop: '10px' }} // Agrega margen superior entre las Toasts
-                >
-                  <Toast.Header>
-                    <strong className="mr-auto">Sugerencia</strong>
-                  </Toast.Header>
-                  <Toast.Body>{toast.suggestion}</Toast.Body>
-                </Toast>
-              ))}
-            </Col>
-    </>
+      </>
   );
 }
 
-export default RightViewer;
+export default RightViewer2;
