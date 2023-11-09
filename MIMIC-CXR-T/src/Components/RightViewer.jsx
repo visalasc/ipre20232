21413b52cc28dda586a815974d1a33133da3c4d4
Valeria Salas <visalas@uc.cl>
2023-11-09 04:05:06 -0300

    fix loadtUserTranslatedPhrase

--- MIMIC-CXR-T/src/Components/RightViewer.jsx
+++ MIMIC-CXR-T/src/Components/RightViewer.jsx
@@ -1,40 +1,80 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Row, Col, Card, ToggleButton} from 'react-bootstrap';
 import './rightviewer.css';
 import ModalSuggestions from './ModalSuggestions';
+import { createUserTranslatedPhrase, deleteCorrection, deleteSuggestion, 
+  getPreviousUserTranslatedPhrase, updateUserTranslatedPhrase } from '../utils/api';
+import { AuthContext } from '../auth/AuthContext';
 
 function RightViewer({ translatedreports, currentIndex, highlightedPhraseIndex, setHighlightedPhraseIndex }) {
   const currentTranslatedReport = translatedreports[currentIndex];
   const [translatedPhrasesState, setTranslatedPhrasesState] = useState({})
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTranslatedPhraseId, setSelectedTranslatedPhraseId] = useState(null);
+  const { token } = useContext(AuthContext);
 
-
-  const loadUserTranslatedPhrase = async () => {
-    //falta completar esta funcion para traer los user translatedphrase y asi dar estado true o false a los botones check y times al abrir el reportgroupreprot
-  };
+  const loadUserTranslatedPhrase = async (translatedphrase) => {
+      try {
+        const response = await getPreviousUserTranslatedPhrase(translatedphrase.id, token);
+        if (response) {
+          if (response.isSelectedCheck) {
+            setTranslatedPhrasesState(prev => ({ ...prev, [translatedphrase.id]: true }));
+          } else {
+            setTranslatedPhrasesState(prev => ({ ...prev, [translatedphrase.id]: false }));
+          }
+          
+        }
+      } catch (error) {
+        // Manejar el error, por ejemplo, mostrar un mensaje de error o registrar en la consola
+        console.error(`Error obteniendo traducciones para ${translatedphrase.id}:`, error);
+      }
+  }
 
   const handleTranslatedPhraseClick = (translatedPhrases, check) => {
     if(check) {
+      if (translatedPhrases.id in translatedPhrasesState){
+        updateUserTranslatedPhrase(translatedPhrases.id, true, false, token);
+        console.log("updateUserTranslatedPhrase frase:", translatedPhrases.id)
+        deleteCorrection(translatedPhrases.id, token);
+        console.log("deletedCorrection para frase ", translatedPhrases.id)
+        deleteSuggestion(translatedPhrases.id, token);
+        console.log("deletedSuggestion para frase ", translatedPhrases.id)
+      }
+      else {
+        createUserTranslatedPhrase(translatedPhrases.id, true, false, token);
+        
+      }
       setTranslatedPhrasesState(prev => ({...prev, [translatedPhrases.id]: true}))
     }
     else {
+      if (translatedPhrases.id in translatedPhrasesState){
+        updateUserTranslatedPhrase(translatedPhrases.id, false, true, token);
+        console.log("updateUserTranslatedPhrase frase:", translatedPhrases.id)
+      }
+      else{
+        createUserTranslatedPhrase(translatedPhrases.id, false, true, token);
+        console.log("createUserTranslatedPhrase frase:", translatedPhrases.id)
+        
+      }
       setTranslatedPhrasesState(prev => ({...prev, [translatedPhrases.id]: false}))
       setSelectedTranslatedPhraseId(translatedPhrases.id);
       setIsModalOpen(true);
     }
   }
+
   return (
     <>
       <Card text="dark" bg="light" border="secondary">
         <Card.Header>Reporte Pre-traducido:</Card.Header>
-        <Card.Text>TranslatedReportID: {currentTranslatedReport.id}</Card.Text>
+        <Card.Text>TranslatedReportID: {currentTranslatedReport.id}</Card.Text> 
         <Row>
-          <Col>
-            {currentTranslatedReport.translatedphrases.map((translatedphrase, indexPhrase) => (
-             
+            <Col>
+          {currentTranslatedReport.translatedphrases.map((translatedphrase, indexPhrase) => {
+              loadUserTranslatedPhrase(translatedphrase); 
+
+              return (
               <div key={translatedphrase.id} className="translated-phrase">
                 <Card.Text className={`${translatedphrase.isCorrect ||
                   translatedphrase.isIncorrect ? 'selected' : ''}
@@ -61,7 +101,8 @@ function RightViewer({ translatedreports, currentIndex, highlightedPhraseIndex, 
                   </ToggleButton>
                 </div>
               </div>
-            ))}
+              );
+                })}
           </Col>
         </Row>
       </Card>
