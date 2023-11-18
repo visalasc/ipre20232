ed312891b75c2f8142a5038d55a3af2088e6400e
Valeria Salas <visalas@uc.cl>
2023-11-18 05:37:12 -0300

    fix progresbar

--- MIMIC-CXR-T/src/common/ReportGroupSelection.jsx
+++ MIMIC-CXR-T/src/common/ReportGroupSelection.jsx
@@ -1,38 +1,40 @@
 import { useState, useEffect, useContext } from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, ProgressBar } from 'react-bootstrap';
 import NavBarReportSelection from '../Components/NavBarReportSelect';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import axios from 'axios';
 import { AuthContext } from '../auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import { getReportGroupsByUser, getUserReportGroup } from '../utils/api';
 
 const ReportGroupSelection = () => {
   const { token } = useContext(AuthContext);
   const [reportGroups, setReportGroups] = useState([]);
+  const [progress, setProgress] = useState({});
   const navigate = useNavigate();
 
   const handleSelectButtonClick = (groupId) => {
     navigate(`/translator/${groupId}`);
   };
-  
+
+  const fetchUserReportGroupProgress = async (reportGroupId) => {
+    try {
+      const response = await getUserReportGroup(reportGroupId, token);
+      if (response) {
+        let progressReportGroup = response.progress;
+        setProgress((prevProgress) => ({ ...prevProgress, [reportGroupId]: progressReportGroup }));
+      }
+      // Devuelve la promesa resultante
+      return response;
+    } catch (error) {
+      console.error('Error fetching user report group:', error);
+      throw error; // Propaga el error para que Promise.all lo maneje
+    }
+  };
+
   useEffect(() => {
     const fetchUserReportGroups = async () => {
       try {
-        if (!token) {
-          console.error('Token not available.');
-          return;
-        }
-
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        };
-
-        const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/reportgroups/user/${token}`, config);
-       
-        setReportGroups(response.data);
+        const response = await getReportGroupsByUser(token);
+        setReportGroups(response);
       } catch (error) {
         console.error('Error fetching report groups:', error);
       }
@@ -41,6 +43,18 @@ const ReportGroupSelection = () => {
     fetchUserReportGroups();
   }, [token]);
 
+  useEffect(() => {
+    const fetchProgressForAllGroups = async () => {
+      const promises = reportGroups.map((group) =>
+        fetchUserReportGroupProgress(group.id)
+      );
+
+      await Promise.all(promises);
+    };
+
+    fetchProgressForAllGroups();
+  }, [reportGroups, token]);
+  
   return (
     <div>
       <NavBarReportSelection />
@@ -48,35 +62,28 @@ const ReportGroupSelection = () => {
       <Container>
         <Row>
           <Col>
-          <Card>
+          <Card style={{ width: '35rem' }}>
               <Row>
-                <Col><Card.Text>Id</Card.Text></Col>
-                <Col><Card.Text>Fecha de creación</Card.Text></Col>
-                <Col><Card.Text>Reportes por traducir</Card.Text></Col>
-                 <Col><Card.Text>Progreso</Card.Text></Col>
-                <Col><Card.Text>Seleccionar</Card.Text></Col>
+                <Col xs={1}><Card.Text>Id</Card.Text></Col>
+                <Col  xs={4}><Card.Text>Fecha de creación</Card.Text></Col>
+                <Col  xs={4}><Card.Text>Reportes por traducir</Card.Text></Col>
+                <Col  xs={3}><Card.Text>Seleccionar</Card.Text></Col>
               </Row>
             </Card>
           </Col>
         </Row>
-        {reportGroups.map((group, index) => (
+        {reportGroups.map((group, index) => ( 
           <Row key={index}>
             <Col>
             <Card>
               <Row>
                 <Col><Card.Text>{group.id}</Card.Text></Col>
-                <Col><Card.Text>{group.createdAt}</Card.Text></Col>
-                <Col><Card.Text>X</Card.Text></Col>
-                <Col><Card.Text>{group.numberOfReportsToTranslate}</Card.Text></Col>
-                  <Col>
-                  <ProgressBar
-                    variant="success"
-                    now={(group.numberOfReportsTranslated / group.numberOfReportsToTranslate) * 100}
-                    label={`${Math.round((group.numberOfReportsTranslated / group.numberOfReportsToTranslate) * 100)}%`}
-                  />
+                <Col><Card.Text>
+                  {(group.createdAt).slice(8,10)+(group.createdAt).slice(4,8)+(group.createdAt).slice(0,4)}
+                  </Card.Text></Col>
+                  <Col><ProgressBar striped animated variant="success" now={progress[group.id]} label={`${Math.round(progress[group.id])}%`} />
                 </Col>
                 <Col>
-                  
                   <Button variant="primary" onClick={() => handleSelectButtonClick(group.id)}>Seleccionar</Button>
                 </Col>
               </Row>
