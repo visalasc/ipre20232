5db0188d3dbfde7d68c74c92ab34e509e4a61bb8
Valeria Salas <visalas@uc.cl>
2024-01-26 12:02:28 -0300

    feat use authprovider admin routes

--- MIMIC-CXR-T/src/Components/NavAdmin.jsx
+++ MIMIC-CXR-T/src/Components/NavAdmin.jsx
@@ -1,15 +1,20 @@
 import { Nav, Navbar, Button } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
+import { useAuth } from '../auth/AuthProvider';
 
 function NavAdmin() {
+  const { user } = useAuth();
+
   return (
     <Navbar className="justify-content-end" bg="dark" data-bs-theme="dark" expand="lg">
     <Nav className="ml-auto"> 
-      <Nav.Link href="/admin">
-        <Button variant="info">
-          vista admin
-        </Button>
-      </Nav.Link>
+        {user && user.role === 'Admin' && (
+              <Nav.Link href="/admin">
+                <Button variant="info">
+                  Vista admin
+                </Button>
+              </Nav.Link>
+            )}
       <Nav.Link href="/reportselection">
         <Button variant="success">
             Vista usuario
--- MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
+++ MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
@@ -1,17 +1,22 @@
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
 import ModalUploadReport from '../Components/ModalUploadReport';
+import { useAuth } from '../auth/AuthProvider';  // Importa el contexto de autenticación
 
 const NavBarReportSelection = () => {
+  const { user } = useAuth();
+
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Navbar.Brand >MIMIC-CXR</Navbar.Brand>
       <Nav className="ml-auto"> 
-        <Nav.Link href="/admin">
-          <Button variant="info">
-            Vista admin
-          </Button>
-        </Nav.Link>
+        {user && user.role === 'Admin' && (
+          <Nav.Link href="/admin">
+            <Button variant="info">
+              Vista admin
+            </Button>
+          </Nav.Link>
+        )}
         <Nav.Link href="/reportselection">
           <Button variant="success">
             Home
