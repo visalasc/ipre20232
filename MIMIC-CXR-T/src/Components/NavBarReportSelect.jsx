94df66707220a14c4bfcc882acf261cc52d0d31d
Valeria Salas <visalas@uc.cl>
2024-01-25 22:28:24 -0300

    feat admin nav

--- MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
+++ MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
@@ -2,24 +2,26 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
 import ModalUploadReport from '../Components/ModalUploadReport';
 
-
 const NavBarReportSelection = () => {
-
-
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Navbar.Brand >MIMIC-CXR</Navbar.Brand>
       <Nav className="ml-auto"> 
+        <Nav.Link href="/admin">
+          <Button variant="info">
+            Vista admin
+          </Button>
+        </Nav.Link>
         <Nav.Link href="/reportselection">
           <Button variant="success">
-            Seleccionar Reportes
+            Home
           </Button>
         </Nav.Link>
         <Nav.Link>
           <ModalUploadReport/>
         </Nav.Link>
         <Nav.Link>
-        <LogoutButton />
+          <LogoutButton />
         </Nav.Link>
       </Nav>
     </Navbar>
