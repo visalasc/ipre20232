beaa90c097ce0b0a1989310bfe1658a571986c22
Valeria Salas <visalas@uc.cl>
2024-01-26 01:32:18 -0300

    fix config

--- MIMIC-CXR-T/vite.config.js
+++ MIMIC-CXR-T/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  ssr: false, // Agrega esta línea
+  build: {
+    // Otras opciones de construcción si las necesitas
+  },
+});
