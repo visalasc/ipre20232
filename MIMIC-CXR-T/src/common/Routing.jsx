a14f39ecf30dbf8986822d0aba4452862849c697
Valeria Salas <visalas@uc.cl>
2024-01-26 11:47:02 -0300

    fix adminroute

--- MIMIC-CXR-T/src/common/Routing.jsx
+++ MIMIC-CXR-T/src/common/Routing.jsx
@@ -6,6 +6,23 @@ import Signup from '../profile/Signup'
 import ReportGroupSelection from '../common/ReportGroupSelection'; 
 import Admin from '../common/Admin';
 import TableUserDisplayReportGroup from '../Components/TableUserDisplayReportGroup'
+import { useAuth } from '../auth/AuthProvider';  // Importa el contexto de autenticación
+import { useNavigate } from 'react-router-dom';
+
+function AdminRoute({ element }) {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  // Comprueba si el usuario tiene el rol de administrador
+  if (user && user.role === 'Admin') {
+    return element;
+  } else {
+    // Si el usuario no es un administrador, redirige a una página diferente
+    navigate('/');
+    // Puedes retornar null o algún componente informativo aquí si lo prefieres
+    return null;
+  }
+}
 
 function Routing() {
     return(
@@ -17,7 +34,7 @@ function Routing() {
             <Route path={"/login"} element={<ModalLogin />}/>
             <Route path={"/signup"} element={<Signup />}/>
             <Route path={"/reportselection"} element={<ReportGroupSelection />} />
-            <Route path={"/admin"} element={<Admin />}/>
+            <AdminRoute path={"/admin"} element={<Admin />} />
             
         </Routes>
     </BrowserRouter>)
