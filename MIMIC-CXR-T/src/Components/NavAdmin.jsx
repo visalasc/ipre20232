65c9713c1ab71015224b1addb1b942f3a8be43bb
Valeria Salas <visalas@uc.cl>
2024-01-26 12:39:21 -0300

    fix auth

--- MIMIC-CXR-T/src/Components/NavAdmin.jsx
+++ MIMIC-CXR-T/src/Components/NavAdmin.jsx
@@ -2,25 +2,21 @@ import { Nav, Navbar, Button } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
 
 function NavAdmin() {
-  const user = JSON.parse(localStorage.getItem('user'));
-
   return (
     <Navbar className="justify-content-end" bg="dark" data-bs-theme="dark" expand="lg">
     <Nav className="ml-auto"> 
-        {user && user.role === 'Admin' && (
-              <Nav.Link href="/admin">
-                <Button variant="info">
-                  Vista admin
-                </Button>
-              </Nav.Link>
-            )}
-      <Nav.Link href="/reportselection">s
+      <Nav.Link href="/admin">
+        <Button variant="info">
+          vista admin
+        </Button>
+      </Nav.Link>
+      <Nav.Link href="/reportselection">
         <Button variant="success">
             Vista usuario
         </Button>
       </Nav.Link>
       <Nav.Link>
-        <LogoutButton />
+        <LogoutButton/>
       </Nav.Link>
     </Nav>
   </Navbar>
--- MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
+++ MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
@@ -3,19 +3,10 @@ import LogoutButton from '../profile/Logout';
 import ModalUploadReport from '../Components/ModalUploadReport';
 
 const NavBarReportSelection = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
-
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Navbar.Brand >MIMIC-CXR</Navbar.Brand>
       <Nav className="ml-auto"> 
-        {user && user.role === 'Admin' && (
-          <Nav.Link href="/admin">
-            <Button variant="info">
-              Vista admin
-            </Button>
-          </Nav.Link>
-        )}
         <Nav.Link href="/reportselection">
           <Button variant="success">
             Home
