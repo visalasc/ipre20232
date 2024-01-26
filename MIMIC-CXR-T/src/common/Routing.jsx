5c6d1e06f783d26ad2ccbdebf40ff7926b571b4d
Valeria Salas <visalas@uc.cl>
2024-01-25 22:25:21 -0300

    feat routing admin

--- MIMIC-CXR-T/src/common/Routing.jsx
+++ MIMIC-CXR-T/src/common/Routing.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from '../common/App'
-import Translator from '../common/Translator'
+import Translator from '../common/Translator2'
 import ModalLogin from '../profile/ModalLogin'
 import Signup from '../profile/Signup'
-import AdminCheck from '../protected/AdminCheck'
-import UserCheck from '../protected/UserCheck'
 import ReportGroupSelection from '../common/ReportGroupSelection'; 
+import Admin from '../common/Admin';
+import TableUserDisplayReportGroup from '../Components/TableUserDisplayReportGroup'
 
 function Routing() {
     return(
     <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<App />}/>
-            <Route path={'/translator/:groupId'} element={<Translator/>}/>
+            <Route path={'/translator/:groupId/'} element={<Translator/>}/>
+            <Route path={'/tablereportgroup/:groupId'} element={<TableUserDisplayReportGroup/>}/>
             <Route path={"/login"} element={<ModalLogin />}/>
             <Route path={"/signup"} element={<Signup />}/>
-            <Route path={"/admincheck"} element={<AdminCheck />}/>
-            <Route path={"/usercheck"} element={<UserCheck />}/>
-            <Route path={"/reportselection"} element={<ReportGroupSelection />}/>
-
+            <Route path={"/reportselection"} element={<ReportGroupSelection />} />
+            <Route path={"/admin"} element={<Admin />}/>
+            
         </Routes>
     </BrowserRouter>)
 }
