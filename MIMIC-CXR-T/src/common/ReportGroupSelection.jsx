695c6b51bcd35ff4dfc2259fb8a051ce145e2c17
Valeria Salas <visalas@uc.cl>
2024-01-25 22:20:39 -0300

    feat progress bar report

--- MIMIC-CXR-T/src/common/ReportGroupSelection.jsx
+++ MIMIC-CXR-T/src/common/ReportGroupSelection.jsx
@@ -1,45 +1,37 @@
 import { useState, useEffect, useContext } from 'react';
-import {Row, Button, ProgressBar, Form, Table } from 'react-bootstrap';
+import { Row, Button, Table, ProgressBar } from 'react-bootstrap';
 import NavBarReportSelection from '../Components/NavBarReportSelect';
 import { AuthContext } from '../auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { getReportGroupsByUser, getUserReportGroup } from '../utils/api';
+
 import './reportgroupselection.css';
+
 const ReportGroupSelection = () => {
   const { token } = useContext(AuthContext);
   const [reportGroups, setReportGroups] = useState([]);
-  const [progress, setProgress] = useState({});
+  const [reportProgress, setReportProgress] = useState({});
+  const [sortColumn, setSortColumn] = useState('id');
+  const [sortDirection, setSortDirection] = useState('asc');
+  const [progressSortDirection, setProgressSortDirection] = useState('asc');
 
-  const [filterPercentageMin, setFilterPercentageMin] = useState(0);
-  const [filterPercentageMax, setFilterPercentageMax] = useState(100);
-  const [hideCompleted, setHideCompleted] = useState(false);
- 
   const navigate = useNavigate();
 
-  const handleSelectButtonClick = (groupId) => {
-    navigate(`/translator/${groupId}`);
+  const handleSelectButtonClick = (groupId, reportProgress) => {
+    const reportProgressPercentage = Math.round(reportProgress);
+    navigate(`/translator/${groupId}`, { state: { reportProgressPercentage } });
   };
 
-  const handleFilterInputChangeMin = (event) => {
-    const percentage = parseFloat(event.target.value);
-    setFilterPercentageMin(percentage);
-  };
-
-  const handleFilterInputChangeMax = (event) => {
-    const percentage = parseFloat(event.target.value);
-    setFilterPercentageMax(percentage);
-  };
-
-  const handleHideCompletedButtonClick = () => {
-    setHideCompleted(true);
+  const viewTableUserDisplayReportGroup = (groupId) => {
+    navigate(`/tablereportgroup/${groupId}`);
   };
 
   const fetchUserReportGroupProgress = async (reportGroupId) => {
     try {
       const response = await getUserReportGroup(reportGroupId, token);
       if (response) {
-        let progressReportGroup = response.progress;
-        setProgress((prevProgress) => ({ ...prevProgress, [reportGroupId]: progressReportGroup }));
+        let progressReportGroup = response.progressReports;
+        setReportProgress((prevProgress) => ({ ...prevProgress, [reportGroupId]: progressReportGroup }));
       }
       // Devuelve la promesa resultante
       return response;
@@ -54,7 +46,6 @@ const ReportGroupSelection = () => {
       try {
         const response = await getReportGroupsByUser(token);
         setReportGroups(response);
-        console.log("response: ",response)
       } catch (error) {
         console.error('Error fetching report groups:', error);
       }
@@ -75,87 +66,85 @@ const ReportGroupSelection = () => {
     fetchProgressForAllGroups();
   }, [reportGroups, token]);
 
-  const filteredReportGroups = reportGroups.filter(
-    (group) =>
-      (filterPercentageMin <= progress[group.id] && progress[group.id] <= filterPercentageMax) &&
-      (!hideCompleted || progress[group.id] < 100)
-  );
+  const sortGroups = () => {
+    const sortedGroups = [...reportGroups].sort((a, b) => {
+      if (sortColumn === 'id') {
+        return (a.id - b.id) * (sortDirection === 'asc' ? 1 : -1);
+      } else if (sortColumn === 'progress') {
+        return (Math.round(reportProgress[a.id]) - Math.round(reportProgress[b.id])) * (progressSortDirection === 'asc' ? 1 : -1);
+      }
+      return 0;
+    });
+
+    return sortedGroups;
+  };
+
+  const handleColumnHeaderClick = (column) => {
+    if (column === sortColumn) {
+      // Si se hace clic en la misma columna, cambiar la dirección
+      setSortDirection((prevDirection) => (prevDirection === 'asc' ? 'desc' : 'asc'));
+    } else {
+      // Si se hace clic en una nueva columna, cambiar la columna y establecer la dirección a ascendente
+      setSortColumn(column);
+      setSortDirection('asc');
+    }
+  };
+
+  const handleProgressColumnHeaderClick = () => {
+    setSortColumn('progress');
+    setSortDirection('asc');
+    setProgressSortDirection((prevDirection) => (prevDirection === 'asc' ? 'desc' : 'asc'));
+  };
 
   return (
     <div>
       <NavBarReportSelection />
       <h3>Selecciona un grupo de reportes a traducir:</h3>
-    
-        <Table striped borderless hover size="sm" variant="info" className="custom-table">
-        <thead>
-          <tr>
-            <th className="w-75">Filtrar grupos de reportes por % de progreso:</th>
-            <th className="w-25"> <Button
-              size="sm"
-              variant="secondary"
-              onClick={handleHideCompletedButtonClick}
-            >
-              Ocultar completados
-            </Button></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="w-50">
-            <Form.Group>
-                  <Form.Label>% mínimo:</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder="0"
-                    value={filterPercentageMin}
-                    onChange={handleFilterInputChangeMin}
-                  />
-                </Form.Group>
-              </td>
-              <td className="w-50">
-                <Form.Group>
-                  <Form.Label>% máximo:</Form.Label>
-                  <Form.Control
-                    type="number"
-                    placeholder="100"
-                    value={filterPercentageMax}
-                    onChange={handleFilterInputChangeMax}
-                  />
-                </Form.Group>
-              </td>
+      <Row>
+        <Table striped bordered hover variant="primary" responsive='sm' className="custom-table">
+          <thead>
+            <tr>
+              <th className="w-10" onClick={() => handleColumnHeaderClick('id')}>
+                ID {sortColumn === 'id' && <span>{sortDirection === 'asc' ? '↑' : '↓'}</span>}
+              </th>
+              <th className="w-50" onClick={handleProgressColumnHeaderClick}>
+                Progreso {sortColumn === 'progress' && <span>{progressSortDirection === 'asc' ? '↑' : '↓'}</span>}
+              </th>
+              <th className="w-50">Fecha de creación</th>
+              {/* Otros encabezados de columnas si los hay */}
+              <th className="w-50">Traducir</th>
+              <th className="w-50">Ver Reportes</th>
             </tr>
+          </thead>
+          <tbody>
+            {sortGroups().map((group, index) => (
+              <tr key={index}>
+                <td className="w-10">{group.id}</td>
+                <td className="w-50">
+                  <ProgressBar striped animated className="custom-progress-bar"
+                    now={Math.round(reportProgress[group.id])}
+                    label={`${Math.round(reportProgress[group.id])}%`}
+                    variant={
+                      Math.round(reportProgress[group.id]) <= 33 ? "danger" :
+                      Math.round(reportProgress[group.id]) <= 99 ? "warning" :
+                      "success"
+                    } />
+                </td>
+                <td className="w-50">
+                  {(group.createdAt).slice(8,10)+(group.createdAt).slice(4,8)+(group.createdAt).slice(0,4)}
+                </td>
+                {/* Otros datos de la fila si los hay */}
+                <td className="w-50">
+                  <Button variant="primary" onClick={() => handleSelectButtonClick(group.id, reportProgress[group.id])}>Traducir</Button>
+                </td>
+                <td className="w-50">
+                  <Button variant="secondary" onClick={() => viewTableUserDisplayReportGroup(group.id)}>Reportes</Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
-
-        <Row>
-          <Table striped bordered hover variant="primary" responsieve='sm' className="custom-table">
-            <thead>
-             
-            </thead>
-            <tbody>
-            <tr>
-                <td className="w-15">Id</td>
-                <td className="w-25">Fecha de creación</td>
-                <td className="w-50">Progreso</td>
-                <td className="w-25">Seleccionar</td>
-              </tr>
-              {filteredReportGroups.map((group, index) => (
-              <tr key={index} >
-                <td className="w-15">{group.id}</td>
-                <td className="w-25">
-                  {(group.createdAt).slice(8,10)+(group.createdAt).slice(4,8)+(group.createdAt).slice(0,4)}
-                  </td>
-                  <td className="w-50"><ProgressBar striped animated variant="success" now={progress[group.id]} label={`${Math.round(progress[group.id])}%`} />
-                </td>
-                <td className="w-25">
-                  <Button variant="primary" onClick={() => handleSelectButtonClick(group.id)}>Seleccionar</Button>
-                </td>
-              </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Row>
-    
+      </Row>
     </div>
   );
 };
