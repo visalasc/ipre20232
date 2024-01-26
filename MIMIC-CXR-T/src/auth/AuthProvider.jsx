70cee26b7b96522d333a3a2b199edad0e89c04ce
Valeria Salas <visalas@uc.cl>
2024-01-26 12:07:00 -0300

    fix authprovider

--- MIMIC-CXR-T/src/auth/AuthProvider.jsx
+++ MIMIC-CXR-T/src/auth/AuthProvider.jsx
@@ -1,21 +1,28 @@
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 
 function AuthProvider({ children }) {
-    const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
 
-    function logout() {
-        setToken(null)
-    }
+  function logout() {
+    setToken(null);
+    setUser(null);
+  }
 
-    useEffect(() => {
-        localStorage.setItem('token', token);
-    }, [token]);
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
 
-    return (
-        <AuthContext.Provider value={{ token, setToken, logout}}>
-            {children}
-        </AuthContext.Provider>
-    );
-    }
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken, user, setUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
 export default AuthProvider;
