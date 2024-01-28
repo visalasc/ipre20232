9e1ed89dd8fd74ee3aaa1285b2d2b0a302034787
Valeria Salas <visalas@uc.cl>
2024-01-28 01:33:33 -0300

    fix admin

--- MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
+++ MIMIC-CXR-T/src/Components/NavBarReportSelect.jsx
@@ -1,23 +1,41 @@
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import LogoutButton from '../profile/Logout';
 import ModalUploadReport from '../Components/CreateJsonBatchReports';
+import { useNavigate } from 'react-router-dom';
 
 const NavBarReportSelection = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate();
+
+  const isAdmin = () => user && user.role === 'Admin';
+
+  const handleAdminButtonClick = () => {
+    // Redirige a la ruta '/admin' si el usuario es administrador
+    if (isAdmin()) {
+      navigate('/admin');
+    }
+  };
+
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
-      <Navbar.Brand >MIMIC-CXR</Navbar.Brand>
-      <Nav className="ml-auto"> 
+      <Navbar.Brand>MIMIC-CXR</Navbar.Brand>
+      <Nav className="ml-auto">
         <Nav.Link href="/reportselection">
-          <Button variant="success">
-            Home
-          </Button>
+          <Button variant="success">Home</Button>
         </Nav.Link>
         <Nav.Link>
-          <ModalUploadReport/>
+          <ModalUploadReport />
         </Nav.Link>
         <Nav.Link>
           <LogoutButton />
         </Nav.Link>
+        {isAdmin() && (
+          <Nav.Link>
+            <Button variant="primary" onClick={handleAdminButtonClick}>
+              Vista Admin
+            </Button>
+          </Nav.Link>
+        )}
       </Nav>
     </Navbar>
   );
--- MIMIC-CXR-T/src/common/Routing.jsx
+++ MIMIC-CXR-T/src/common/Routing.jsx
@@ -1,42 +1,45 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from '../common/App'
-import Translator from '../common/Translator'
-import ModalLogin from '../profile/ModalLogin'
-import Signup from '../profile/Signup'
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import App from '../common/App';
+import Translator from '../common/Translator';
+import ModalLogin from '../profile/ModalLogin';
+import Signup from '../profile/Signup';
 import ReportGroupSelection from '../common/ReportGroupSelection'; 
 import Admin from '../common/Admin';
-import TableUserDisplayReportGroup from '../Components/TableUserDisplayReportGroup'
-import { useNavigate } from 'react-router-dom';
+import TableUserDisplayReportGroup from '../Components/TableUserDisplayReportGroup';
 
-function AdminRoute({ element }) {
-  const user = JSON.parse(localStorage.getItem('user'));
+function NoPermissions() {
+  return <div>No tienes permisos para acceder a esta página.</div>;
+}
+
+function AdminComponent() {
   const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('user'));
 
-  // Comprueba si el usuario tiene el rol de administrador
-  if (user && user.role === 'Admin') {
-    return element;
-  } else {
-    // Si el usuario no es un administrador, redirige a una página diferente
-    navigate('/');
-    // Puedes retornar null o algún componente informativo aquí si lo prefieres
-    return null;
+  // Función para comprobar si el usuario es administrador
+  const isAdmin = () => user && user.role === 'Admin';
+
+  // Redirigir a la página principal si el usuario no es administrador
+  if (!isAdmin()) {
+    return <Navigate to="/" />;
   }
+
+  return <Admin />;
 }
 
 function Routing() {
-    return(
+  return (
     <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<App />}/>
-            <Route path={'/translator/:groupId/'} element={<Translator/>}/>
-            <Route path={'/tablereportgroup/:groupId'} element={<TableUserDisplayReportGroup/>}/>
-            <Route path={"/login"} element={<ModalLogin />}/>
-            <Route path={"/signup"} element={<Signup />}/>
-            <Route path={"/reportselection"} element={<ReportGroupSelection />} />
-            <AdminRoute path={"/admin"} element={<Admin />} />
-            
-        </Routes>
-    </BrowserRouter>)
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/translator/:groupId/" element={<Translator />} />
+        <Route path="/tablereportgroup/:groupId" element={<TableUserDisplayReportGroup />} />
+        <Route path="/login" element={<ModalLogin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/reportselection" element={<ReportGroupSelection />} />
+        <Route path="/admin" element={<AdminComponent />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Routing;
