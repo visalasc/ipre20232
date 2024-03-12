69ed17cba283cb4a214be4537c3b15d1fffd9817
Valeria Salas <visalas@uc.cl>
2024-03-12 11:04:36 -0300

    delete register component in app

--- MIMIC-CXR-T/src/common/App.jsx
+++ MIMIC-CXR-T/src/common/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Signup from '../profile/Signup';
 import MyNavBar from '../Components/Nav';
 
 
@@ -13,12 +12,10 @@ function App() {
           <p> El propósito de esta plataforma es traducir el conjunto de datos MIMIC-CXR del inglés al español. Los profesionales médicos seleccionados pueden introducir y corregir traducciones realizadas por computadora para volver a capacitarse en el futuro y mejorar el rendimiento de la traducción.</p>
       
           <p> The purpose of this platform is to translate the MIMIC-CXR dataset from English to Spanish. Selected medical professionals can input and correct computer-made translations to be retrained in the future and improve translation performance.</p>
+          <p> FONDECYT Regular 1231724 </p>
+          <p> Multimodal, multitask and trasfer learning for Deep radiological report generation</p>
          </section>
 
-
-        <section className="right-section">
-          <Signup />
-        </section>
       </main>
 
       <footer>
