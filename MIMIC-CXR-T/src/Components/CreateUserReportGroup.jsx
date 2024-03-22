a1d72caf2946ddc04eaa7d12fb29bdbad6769725
Valeria Salas <visalas@uc.cl>
2024-03-22 02:32:22 -0300

    feat fetchUserReportGroup

--- MIMIC-CXR-T/src/Components/CreateUserReportGroup.jsx
+++ MIMIC-CXR-T/src/Components/CreateUserReportGroup.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
 import { Form, Button, ListGroup, Table, Col, Row, Alert  } from 'react-bootstrap';
 import { AuthContext } from '../auth/AuthContext';
-import { getAllUsers} from '../utils/api';
+import { getAllUsers, getUserReportGroups} from '../utils/api';
 
 const CreateUserReportGroup = ({ onCreateUserReportGroup, reportGroupReports }) => {
   const { token } = useContext(AuthContext);
@@ -29,6 +29,38 @@ const CreateUserReportGroup = ({ onCreateUserReportGroup, reportGroupReports }) 
 
     fetchUsers();
   }, []);
+
+  const fetchUsersForReportGroup = async (reportGroupId) => {
+    try {
+      const userReportGroup = await getUserReportGroups(reportGroupId, token);
+      
+      return userReportGroup.map((group) => group.userId);
+    } catch (error) {
+      console.error('Error fetching users for report group:', error);
+      return [];
+    }
+  };
+
+  const fetchAndUpdateUsersForReports = async () => {
+    const updatedReportsWithUsers = await Promise.all(
+      updatedReports.map(async (report) => {
+        try {
+          const usersForReportGroup = await fetchUsersForReportGroup(report.id);
+          return { ...report, users: usersForReportGroup };
+        } catch (error) {
+          console.error('Error fetching users for report group:', error);
+          return { ...report, users: [] }; // Manejo de errores, establece usuarios como vacío
+        }
+      })
+    );
+    
+    setUpdatedReports(updatedReportsWithUsers);
+  };
+
+  useEffect(() => {
+    fetchAndUpdateUsersForReports();
+  }, [updatedReports]);
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -69,6 +101,8 @@ const CreateUserReportGroup = ({ onCreateUserReportGroup, reportGroupReports }) 
     }
   };
 
+
+
   return (
     <div>
       <Row>
@@ -77,30 +111,42 @@ const CreateUserReportGroup = ({ onCreateUserReportGroup, reportGroupReports }) 
           </Alert>
      <Col md={{offset: 1 }}>
         
-      <Table striped  hover>
-        <thead>
-          <tr>
-            <th>Batch Id</th>
-            <th>Report IDs</th>
-          </tr>
-        </thead>
-        <tbody>
-          {updatedReports.map((reportGroupReport) => (
-            <tr key={reportGroupReport.id}>
-              <td>{reportGroupReport.id}</td>
-              <td>
-                {reportGroupReport.reports && reportGroupReport.reports.length > 0 ? (
-                  reportGroupReport.reports.map((report) => (
-                    <div key={report.id}>{report.id}</div>
-                  ))
-                ) : (
-                  'No reports'
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+   {updatedReports.length > 0 && (
+          <Table striped  hover>
+            <thead>
+              <tr>
+                <th>Batch Id</th>
+                <th>Reports IDs</th>
+                <th>User IDs</th>
+              </tr>
+            </thead>
+            <tbody>
+              {updatedReports.map((reportGroupReport) => (
+                <tr key={reportGroupReport.id}>
+                  <td>{reportGroupReport.id}</td>
+                  <td>
+                    {reportGroupReport.reports && reportGroupReport.reports.length > 0 ? (
+                      reportGroupReport.reports.map((report) => (
+                        <div key={report.id}>{report.id}</div>
+                      ))
+                    ) : (
+                      'No reports'
+                    )}
+                  </td>
+                  <td>
+                    {reportGroupReport.users && reportGroupReport.users.length > 0 ? (
+                      reportGroupReport.users.map((user) => (
+                        <div key={user}>{user}</div>
+                      ))
+                    ) : (
+                      'No users'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
     </Col>
 
       <Col>
@@ -118,8 +164,8 @@ const CreateUserReportGroup = ({ onCreateUserReportGroup, reportGroupReports }) 
           <Form.Group controlId="formGroupUserIds">
             <Form.Label>Seleccionar Usuarios:</Form.Label>
             <ListGroup>
-              {users.map((user) => (
-                <ListGroup.Item key={user.id}>
+            {users.map((user) => (
+                <ListGroup.Item key={`${user.id}-${user.firstName}-${user.lastName}`}>
                   <Form.Check
                     type="checkbox"
                     label={`${user.id} - ${user.firstName} ${user.lastName}`}
