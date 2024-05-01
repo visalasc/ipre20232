7dc739f60a8bced29832217152d00eaec503d660
Manuel Gana <manuel.gana@uc.cl>
2024-04-30 22:10:27 -0400

    DockerFile

--- MIMIC-CXR-T/vite.config.js
+++ MIMIC-CXR-T/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+/*
 export default defineConfig({
   plugins: [react()],
   ssr: false, // Agrega esta línea
@@ -9,3 +10,19 @@ export default defineConfig({
     // Otras opciones de construcción si las necesitas
   },
 });
+*/
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
+ });
