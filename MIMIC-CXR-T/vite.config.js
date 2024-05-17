f2ebddddbe3e0cc87993410a66dea74b8ee97487
Manuel Gana <manuel.gana@uc.cl>
2024-05-17 17:58:59 -0400

    Docker actualizado

--- MIMIC-CXR-T/vite.config.js
+++ MIMIC-CXR-T/vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-/*
-export default defineConfig({
-  plugins: [react()],
-  ssr: false, // Agrega esta línea
-  build: {
-    // Otras opciones de construcción si las necesitas
-  },
-});
-*/
-
 export default defineConfig({
   base: "/",
   plugins: [react()],
