c0aa10b189954966c1f0008c58f6ea885333e3ef
Valeria Salas <visalas@uc.cl>
2024-05-28 15:41:03 -0400

    feat acronymes front

--- MIMIC-CXR-T/src/Components/ModalSuggestionCorrecctions.jsx
+++ MIMIC-CXR-T/src/Components/ModalSuggestionCorrecctions.jsx
@@ -11,8 +11,9 @@ import {
   getPreviousUserCorrections,
   deleteUserCorrectionsTranslatedSentence,
 } from '../utils/api';
-import '../utils/modalScripts';
 import WordSelector from './WordSelector';
+import ToggleButtonGroup from '../Components/ToggleButtonGroup';
+import '../utils/modalScripts';
 import './modal.css';
 
 function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseWithoutSave, onSave }) {
@@ -24,11 +25,43 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
   const [originalSentence, setOriginalSentence] = useState('');
   const [editedTranslatedSentence, setEditedTranslatedSentence] = useState('');
   const [otherErrorDescription, setOtherErrorDescription] = useState('');
-  const { token } = useContext(AuthContext);
   const [selectedOptionsByType, setSelectedOptionsByType] = useState({});
   const [selectedWords, setSelectedWords] = useState([]);
   const [showNoChangesAlert, setShowNoChangesAlert] = useState(false);
-  const [headerHeight, setHeaderHeight] = useState(0); // Estado para almacenar la altura del encabezado
+  const [headerHeight, setHeaderHeight] = useState(0); 
+  const [acrSelectedTerminological, setAcrSelectedTerminological] = useState('');
+  const [acrSelectedGrammatical, setAcrSelectedGrammatical] = useState('');
+  const [acrSelectedFunctional, setAcrSelectedFunctional] = useState('');
+  const [showNoAcrTerminologicalAlert, setShowNoAcrTerminologicalAlert] = useState(false);
+  const [showNoAcrGrammaticalAlert, setShowNoAcrGrammaticalAlert] = useState(false);
+  const [showNoAcrFunctionalAlert, setShowNoAcrFunctionalAlert] = useState(false);
+  const { token } = useContext(AuthContext);
+
+  // Función para manejar el clic en el toggle de acrónimo
+  const handleToggleChange = (value, type) => {
+    switch (type) {
+      case 'terminological':
+        setAcrSelectedTerminological(value);
+        break;
+      case 'grammatical':
+        setAcrSelectedGrammatical(value);
+        break;
+      case 'functional':
+        setAcrSelectedFunctional(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  // Función para manejar el clic en las opciones del WordSelector
+  const handleOptionClick = (options, type) => {
+    setSelectedOptionsByType((prevOptionsByType) => {
+      const updatedOptionsByType = { ...prevOptionsByType };
+      updatedOptionsByType[type] = options;
+      return updatedOptionsByType;
+    });
+  };
 
   const loadSentenceAndTranslation = async (selectedTranslatedSentenceId) => {
     try {
@@ -100,25 +133,89 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
       setPreviousCorrection(null);
     }
   };
-  
-  const handleOptionClick = (options, type) => {
-    setSelectedOptionsByType((prevOptionsByType) => {
-      const updatedOptionsByType = { ...prevOptionsByType };
-      updatedOptionsByType[type] = options;
-      return updatedOptionsByType;
-    });
-  };
 
   const handleModalSave = async (event) => {
     event.preventDefault();
     try {
-      console.log("se guarda desde modal")
+      console.log("se guarda desde modal");
+  
+      // Verificar si se han seleccionado opciones y si se ha seleccionado un acrónimo
+      const isOptionsSelected = Object.keys(selectedOptionsByType).some((key) => selectedOptionsByType[key].length > 0);
+      
+      if (!isOptionsSelected) {
+        // Mostrar alerta y evitar que se guarde el modal si no se cumple alguna de las condiciones
+        setShowNoChangesAlert(true);
+        return;
+      }
+
+      // Verificar si se han seleccionado opciones
+      const isTerminologicalWordSelected = selectedOptionsByType['terminological'].length > 0;
+      const isGrammaticalWordSelected = selectedOptionsByType['grammatical'].length > 0;
+      const isFunctionalWordSelected = selectedOptionsByType['functional'].length > 0;
+
+      // Verificar si se ha seleccionado un acrónimo para cada sección
+      const isAcrToggleSelectedTerminological = acrSelectedTerminological !== null && isTerminologicalWordSelected;
+      const isAcrToggleSelectedGrammatical = acrSelectedGrammatical !== null && isGrammaticalWordSelected;
+      const isAcrToggleSelectedFunctional = acrSelectedFunctional !== null && isFunctionalWordSelected;
+
+      // Verificar si se ha seleccionado si o no en el ToggleButtonGroup de cada sección
+      const isAcrToggleGroupSelectedTerminological = acrSelectedTerminological !== null;
+      const isAcrToggleGroupSelectedGrammatical = acrSelectedGrammatical !== null;
+      const isAcrToggleGroupSelectedFunctional = acrSelectedFunctional !== null;
+
+    // Mostrar alerta si no se cumplen las condiciones requeridas para cada sección
+    if (!isOptionsSelected) {
+      setShowNoChangesAlert(true);
+      return;
+    }
+
+    if (!isAcrToggleSelectedTerminological || !isAcrToggleGroupSelectedTerminological) {
+      setShowNoAcrTerminologicalAlert(true);
+      return;
+    }
+
+    if (!isAcrToggleSelectedGrammatical || !isAcrToggleGroupSelectedGrammatical) {
+      setShowNoAcrGrammaticalAlert(true);
+      return;
+    }
+
+    if (!isAcrToggleSelectedFunctional || !isAcrToggleGroupSelectedFunctional) {
+      setShowNoAcrFunctionalAlert(true);
+      return;
+    }
+
+    if (!isAcrToggleSelectedFunctional || !isAcrToggleGroupSelectedFunctional) {
+      // Validar que se hayan seleccionado errores funcionales antes de mostrar la alerta sobre la falta de acrónimo funcional
+      const isFunctionalWordSelected = selectedOptionsByType['functional'].length > 0;
+      if (isFunctionalWordSelected) {
+        setShowNoAcrFunctionalAlert(true);
+      }
+      return;
+    }
+
+    if (!isAcrToggleSelectedTerminological || !isAcrToggleGroupSelectedTerminological) {
+      // Validar que se hayan seleccionado errores funcionales antes de mostrar la alerta sobre la falta de acrónimo funcional
+      const isTerminologicalWordSelected = selectedOptionsByType['terminological'].length > 0;
+      if (isTerminologicalWordSelected) {
+        setShowNoAcrTerminologicalAlert(true);
+      }
+      return;
+    }
+
+    if (!isAcrToggleSelectedGrammatical || !isAcrToggleGroupSelectedGrammatical) {
+      // Validar que se hayan seleccionado errores funcionales antes de mostrar la alerta sobre la falta de acrónimo funcional
+      const isGrammaticalWordSelected = selectedOptionsByType['grammatical'].length > 0;
+      if (isGrammaticalWordSelected) {
+        setShowNoAcrGrammaticalAlert(true);
+      }
+      return;
+    }
+
+    
       // Check if any field has been modified, including editedTranslatedSentence
-      const isModified =
-        editedTranslatedSentence !== translatedSentence && selectedOptions.length > 0 
+      const isModified = editedTranslatedSentence !== translatedSentence && selectedOptions.length > 0;
   
       if (isModified) {
-      
         if (previousSuggestion === null) {
           // Your existing logic for creating a new suggestion and corrections
           await createSuggestion(
@@ -151,7 +248,6 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
               otherErrorDescription,
               token
             );
-    
           } else {
             await createSuggestion(
               selectedTranslatedSentenceId,
@@ -182,22 +278,40 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
         console.log("selectedTranslatedSentenceId: modal, ", selectedTranslatedSentenceId)
         loadPreviousSuggestionData(selectedTranslatedSentenceId);
       } else {
-        // Handle the case when no field has been modified
-        // For example, show an alert or set an error state
+  
         setShowNoChangesAlert(true);
         console.warn('No changes have been made. Please make modifications before saving.');
-        
       }
+      // Clean up state after saving
+      setSelectedOptions([]);
+      setModalText('');
+      setOtherErrorDescription('');
+      setSelectedOptionsByType({});
+      setSelectedWords([]);
+      setEditedTranslatedSentence('');
+      setAcrSelectedTerminological(null);
+      setAcrSelectedGrammatical(null);
+      setAcrSelectedFunctional(null);
+      onSave();
     } catch (error) {
       console.error('Error saving modal:', error);
       // Handle errors appropriately
     }
   };
   
+  
   const handleModalClose = () => {
     setSelectedOptions([]);
     setPreviousSuggestion(null);
     onHide();
+    setModalText('');
+    setOtherErrorDescription('');
+    setSelectedOptionsByType({});
+    setSelectedWords([]);
+    setEditedTranslatedSentence('');
+    setAcrSelectedTerminological(null);
+    setAcrSelectedGrammatical(null);
+    setAcrSelectedFunctional(null);
     onCloseWithoutSave(); 
   };
 
@@ -223,9 +337,8 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
           setHeaderHeight(headerHeight);
         }
       }} onHide={{}} className="fixed-modal" size="xl" >
-        <Modal.Header className="modal-header">
+        <Modal.Header className="modal-header ">
           <Modal.Title>
-    
             Identificar errores y editar traducción final: 
             <Row className="mx-2 my-2">
                   <Card border="secondary" >
@@ -251,6 +364,11 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
         </Modal.Header>
         <Modal.Body className="modal-body" style={{ marginTop: headerHeight }}>
           <div className="form-container">
+          {showNoChangesAlert && <Alert variant="warning">You must make a change to save.</Alert>}
+          {showNoAcrTerminologicalAlert && <Alert variant="warning">You must select si or no for the terminological section.</Alert>}
+          {showNoAcrGrammaticalAlert && <Alert variant="warning">You must select si or no for the grammatical section.</Alert>}
+          {showNoAcrFunctionalAlert && <Alert variant="warning">You must select si or no for the functional section.</Alert>}
+          
             <Form>
               <Form.Group>
                 <Card border="secondary" className="accordion-container" >      
@@ -279,6 +397,15 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
                               initialSelectedWords={selectedWords}
                               onOptionClick={(option) => handleOptionClick(option, 'terminological')}
                             />
+                            <Col sm="3">
+                              Error considera acrónimo:
+                              <ToggleButtonGroup
+                                  name="terminological"
+                                  options={['si', 'no']}
+                                  value={acrSelectedTerminological}
+                                  onChange={handleToggleChange} 
+                                />
+                            </Col>
                           </Card>
                      
                     </Accordion>
@@ -310,7 +437,15 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
                               onOptionClick={(option) => handleOptionClick(option, 'grammatical')}
 
                             />
-                        
+                              <Col sm="3">
+                              Error considera acrónimo:
+                              <ToggleButtonGroup
+                                  name="grammatical"
+                                  options={['si', 'no']}
+                                  value={acrSelectedGrammatical}
+                                  onChange={handleToggleChange} 
+                                />
+                            </Col>
                       </Card>
                       </Accordion>
                     </Card>
@@ -340,8 +475,16 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
                               selectedOptions={selectedOptionsByType['functional']}
                               initialSelectedWords={selectedWords}
                               onOptionClick={(option) => handleOptionClick(option, 'functional')}
-                            
                             />
+                              <Col sm="3">
+                              Error considera acrónimo:
+                              <ToggleButtonGroup
+                                  name="functional"
+                                  options={['si', 'no']}
+                                  value={acrSelectedFunctional}
+                                  onChange={handleToggleChange} 
+                                />
+                            </Col>
                           </Card>
                        
                     </Accordion>
@@ -351,13 +494,7 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
                       <Accordion.Item eventKey="0">
                         <Accordion.Header>Otros errores</Accordion.Header>
                         <Accordion.Body>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                          aliquip ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                          culpa qui officia deserunt mollit anim id est laborum.
+                        Seleccione este tipo de error si encuentra algún tipo de error que no pertenezca a ninguna de las categorías anteriores. Por ejemplo errores de formato que impiden leer el texto, entre otros
                         </Accordion.Body>
                       </Accordion.Item>
                      
@@ -410,17 +547,7 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
                   />
                 </Row>
               </Form.Group>
-              <Form.Group>
-                <Row className="mx-2 my-2">
-                  <Form.Label className="h5">Si tienes algún comentario adicional agregar aquí:</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={1}
-                    value={modalText}
-                    onChange={(change) => setModalText(change.target.value)}
-                  />
-                </Row>
-              </Form.Group>
+         
             </Form>
           </div>
         </Modal.Body>
@@ -434,6 +561,35 @@ function ModalSuggestions({ show, onHide, selectedTranslatedSentenceId, onCloseW
         >
           Por favor modifique la traducción final antes de guardar.
         </Alert>
+
+        <Alert
+          variant="warning"
+          show={showNoAcrFunctionalAlert}
+          onClose={() => setShowNoAcrFunctionalAlert(false)}
+          dismissible
+        >
+          Por favor seleccione si hay o no errores funcionales por acrónimo antes de guardar.
+        </Alert>
+
+        <Alert
+          variant="warning"
+          show={showNoAcrTerminologicalAlert}
+          onClose={() => setShowNoAcrTerminologicalAlert(false)}
+          dismissible
+        >
+          Por favor seleccione si hay o no errores terminológicos por acrónimo antes de guardar.
+        </Alert>
+
+        <Alert
+          variant="warning"
+          show={showNoAcrGrammaticalAlert}
+          onClose={() => setShowNoAcrGrammaticalAlert(false)}
+          dismissible
+        >
+          Por favor seleccione si hay o no errores gramaticales por acrónimo antes de guardar.
+        </Alert>
+
+
           <Button variant="secondary" onClick={handleModalClose}>
             Cerrar
           </Button>
